10e3ea62df47328ec05ad4d71c31844a825b3483
naifalabdullah211 <naifalabdullah211@gmail.com>
2026-09-12 14:21:09 +0300

    Add public assets and cache update

---
+++ public/sw.js
@@ -0,0 +1,47 @@
+const CACHE = "nasq-code-v3";
+const CORE = ["./", "./manifest.webmanifest", "./brand-icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)),
+        ),
+      ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() =>
+        caches
+          .match(event.request)
+          .then(
+            (cached) =>
+              cached ??
+              (event.request.mode === "navigate"
+                ? caches.match("./")
+                : undefined),
+          ),
+      ),
+  );
+});
